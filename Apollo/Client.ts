550895e2c23613a62086d7bcfa882f17942397ce
grizzle <firbigi1993@gmail.com>
2020-09-03 15:43:54 +0900

    fijx | client uri to heroku

--- Apollo/Client.ts
+++ Apollo/Client.ts
@@ -1,6 +1,7 @@
 import ApolloClient, { InMemoryCache } from "apollo-boost";
 
 export default new ApolloClient({
-	uri: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://seoseohee-server.herokuapp.com",
+	// uri: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://seoseohee-server.herokuapp.com",
+	uri: "https://seoseohee-server.herokuapp.com",
 	cache: new InMemoryCache()
 });
