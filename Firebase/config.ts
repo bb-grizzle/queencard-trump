58f5985e8e190856663b72ed8d70a4f16ea3ba52
grizzle <firbigi1993@gmail.com>
2020-09-10 11:50:59 +0900

    fix git ignore

---
+++ Firebase/config.ts
@@ -0,0 +1,9 @@
+export default {
+	apiKey: "AIzaSyDJLvlz72AoFr_2kZLTL-F8r1j1FF_zqh0",
+	authDomain: "seoseohee-1599490087550.firebaseapp.com",
+	databaseURL: "https://seoseohee-1599490087550.firebaseio.com",
+	projectId: "seoseohee-1599490087550",
+	storageBucket: "seoseohee-1599490087550.appspot.com",
+	messagingSenderId: "674971663984",
+	appId: "1:674971663984:web:4b00e3c4ab291621aafeb5"
+};
