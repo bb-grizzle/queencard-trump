fbe159cb30013dea5d1a23db4f7f9964ec37692f
grizzle <firbigi1993@gmail.com>
2020-09-15 11:07:33 +0900

    fix | mobile icon

--- Layout/MetaLayout.tsx
+++ Layout/MetaLayout.tsx
@@ -28,7 +28,8 @@ const HeadComponent: React.FC = ({ children }) => {
 				<meta property="og:image" content="/thumbnail.png" />
 				<meta property="og:type" content="website" />
 
-				<link rel="apple-touch-icon" href="/public/favicon.ico"></link>
+				<link rel="apple-touch-icon" href="/favicon.ico"></link>
+				<link rel="shortcut icon" href="/favicon.ico"></link>
 			</Head>
 			{children}
 		</>
