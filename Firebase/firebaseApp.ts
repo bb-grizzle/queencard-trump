c603d63c8de167716774d0e03dc7999676a6b328
grizzle <firbigi1993@gmail.com>
2020-12-14 13:20:30 +0900

    test | firebase

--- Firebase/firebaseApp.ts
+++ Firebase/firebaseApp.ts
@@ -6,18 +6,7 @@ import "firebase/storage";
 import "firebase/auth";
 import "firebase/analytics";
 
-const firebaseApp = !firebase.apps.length
-	? firebase.initializeApp({
-			apiKey: "AIzaSyCSn_g0l1s1olchFZFAT9hyw96-oBdDpHs",
-			authDomain: "awesomeschool-ab371.firebaseapp.com",
-			databaseURL: "https://awesomeschool-ab371.firebaseio.com",
-			projectId: "awesomeschool-ab371",
-			storageBucket: "awesomeschool-ab371.appspot.com",
-			messagingSenderId: "837802291128",
-			appId: "1:837802291128:web:ea355d7e578e592fa8294d",
-			measurementId: "G-C58K83G1J3"
-	  })
-	: firebase.app();
+const firebaseApp = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
 export const db = firebaseApp.firestore();
 export const storage = firebaseApp.storage();
