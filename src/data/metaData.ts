646da1af9d65d1719f36c5478db4306d60052179
bb-grizzle <firbigi1993@gmail.com>
2023-06-15 20:52:34 +0900

    INIT

--- src/data/metaData.ts
+++ src/data/metaData.ts
@@ -1,8 +1,8 @@
 const DATA_META = {
-	url: "https://meme-2d572.web.app/",
-	title: "2/12 | OMG-meme",
-	description: "2/12 OMG-meme",
-	site_name: "OMG-meme",
+	url: "http://localhost:3000",
+	title: "6/12 | Trump",
+	site_name: "6/12 | Trump",
+	description: "6/12 | Trump",
 };
 
 export default DATA_META;
